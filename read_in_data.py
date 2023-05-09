d59198ede7b3341d369d09ad7b50895cfc96490b
jojamison <jojamison@davidson.edu>
2023-05-08 20:25:22 -0700

    updated read_in_data.py

--- read_in_data.py
+++ read_in_data.py
@@ -1,47 +1,74 @@
 import cv2
 import numpy as np
 import os
+import pandas as pd
 
 
-path = '/Users/josephjamison/Downloads/data/images/train/'
-folder_list = [i for i in os.listdir(path) if not i.startswith('.')]
+def process_images():
+    path = '/Users/josephjamison/Downloads/data/images/train/'
+    folder_list = [i for i in os.listdir(path) if not i.startswith('.')]
 
-total_images_processed = 0
-
-for first_letter in folder_list:
+    total_images_processed = 0
+    image_idx_pairs = []
     images = []
     labels = []
-    letter_list = [i for i in os.listdir(os.path.join(path, first_letter)) if not i.startswith('.')]
 
-    for folder in letter_list:
-        dir_list = [i for i in os.listdir(os.path.join(path, first_letter, folder)) if not i.startswith('.')]
-        
-        for file in dir_list:
-            try:
-                im = cv2.imread(os.path.join(path, first_letter, folder, file)).transpose((2, 0, 1))
-                images.append(im)
-                labels.append(folder)
-            except:
-                sub_dir_list = [i for i in os.listdir(os.path.join(path, first_letter, folder, file)) 
-                                                                                if not i.startswith('.')]
-                try: 
-                    for file2 in sub_dir_list:
-                        im = cv2.imread(os.path.join(path, first_letter, folder, file, file2)).transpose((2, 0, 1))
-                        images.append(im)
-                        labels.append(folder + "_" + file)
+    for first_letter in folder_list:
+        letter_list = [i for i in os.listdir(os.path.join(path, first_letter)) if not i.startswith('.')]
+        for folder in letter_list:
+            dir_list = [i for i in os.listdir(os.path.join(path, first_letter, folder)) if not i.startswith('.')]
+            for file in dir_list:
+                try:
+                    im = cv2.imread(os.path.join(path, first_letter, folder, file)).transpose((2, 0, 1))
+                    images.append(im)
+                    labels.append(folder)
+                    image_idx_pairs.append([folder + file,  total_images_processed])
+                    total_images_processed += 1
                 except:
-                    print("Warning: One or more folder / file could not be read")
-        
+                    sub_dir_list = [i for i in os.listdir(os.path.join(path, first_letter, folder, file)) 
+                                                                                    if not i.startswith('.')]
+                    try: 
+                        for file2 in sub_dir_list:
+                            im = cv2.imread(os.path.join(path, first_letter, folder, file, file2)).transpose((2, 0, 1))
+                            images.append(im)
+                            labels.append(folder + "_" + file)
+                            image_idx_pairs.append([folder + "_" + file + file2,  total_images_processed])
+                            total_images_processed += 1
+                    except:
+                        print("Warning: One or more folder / file could not be read")
+            
+
+        # images = np.array(images)
+        # labels = np.array(labels)
+
+        # # print(images.shape)
+        # # print(labels.shape)
+        # print(f"Unique labels from folder of letter: {first_letter}")
+        # print(np.unique(labels))
+        # total_images_processed += labels.shape[0]
+        print(f"Just finished folder: {first_letter}. Total number of images processed: {total_images_processed}")
 
     images = np.array(images)
     labels = np.array(labels)
+    image_idx_pairs = np.array(image_idx_pairs)
+    N = images.shape[0]
+    assert N == labels.shape[0]
+    assert N == image_idx_pairs.shape[0]
+    print("images.shape: ", images.shape)
 
-    # print(images.shape)
-    # print(labels.shape)
-    print(f"Unique labels from folder of letter: {first_letter}")
-    print(np.unique(labels))
-    total_images_processed += labels.shape[0]
+    # Write array of image to index pairs to csv
+    image_pairs_DF = pd.DataFrame(image_idx_pairs)
+    image_pairs_DF.to_csv("image_idx_pairs.csv")
+    
+    # Write images and labels to csv 
+    labels_DF = pd.DataFrame(labels)
+    labels_DF.to_pickle("small_images_labels.pkl")
+    image_DF = pd.DataFrame(images.reshape(N, -1))
+    image_DF.to_pickle("small_images_all_data.pkl")
 
-print(f"Total number of images processed: {total_images_processed}")
 
+def main():
+    process_images()
 
+if __name__=="__main__":
+    main()
