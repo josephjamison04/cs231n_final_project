c8dc245f8a3ad147d670ed216c85c28a6e5c621a
Umar Maniku <manikui@stanford.edu>
2023-05-13 04:50:41 +0000

    fc.py finished, with tqdm also

--- fully_connect_baseline/fc.py
+++ fully_connect_baseline/fc.py
@@ -9,8 +9,11 @@ import torch.nn.functional as F
 import torch.optim as optim
 from torch.utils.data import TensorDataset, DataLoader
 from torch.utils.data import sampler
-
 import torchvision.transforms as T
+
+from tqdm import tqdm
+import time, random, numpy as np, argparse
+from types import SimpleNamespace
 
 class FCNet(nn.Module):
     def __init__(self, input_size, num_classes):
@@ -57,15 +60,143 @@ def load_data(train =True,valid = True,test = False):
     # if didn;t load the data, it is None
     return X_train,y_train,X_valid,y_valid,X_test,y_test
 
+def save_model(model, optimizer, args=None, config=None, filepath='fc_base.pt'):
+    save_info = {
+        "model": model.state_dict(),
+        "optim": optimizer.state_dict()
+        # "args": args,
+        # "model_config": config,
+        # "system_rng": random.getstate(),
+        # "numpy_rng": np.random.get_state(),
+        # "torch_rng": torch.random.get_rng_state(),
+        }
+    
+    torch.save(save_info, filepath)
+    print(f"save the model to {filepath}")
+
 def train(args):
     device = torch.device("cuda") if args.use_gpu else torch.device("cpu")
     # load train and valid data
     X_train,y_train,X_valid,y_valid,_ ,_ =load_data()
-    
 
+    X_train = torch.from_numpy(X_train).to(device)
+    X_valid = torch.from_numpy(X_valid).to(device)
+    y_train = torch.from_numpy(y_train).to(device)
+    y_valid = torch.from_numpy(y_valid).to(device)
+
+    train_dataset = TensorDataset(X_train, y_train)
+    val_dataset = TensorDataset(X_valid, y_valid)
+    loader_train = DataLoader(train_dataset, batch_size=args.batch_size)
+    loader_val = DataLoader(val_dataset, batch_size=args.batch_size)
+    print('finished setting dataloaders')
+
+    
+    epochs =args.epochs
     input_size = 3 * 128 * 128  # image size
-    num_classes = 10  # number of classes
-    model = FCNet(input_size, num_classes)
+    num_classes = 100  # number of classes
+    learning_rate = args.lr
+    dtype = torch.float32
+    # model = FCNet(input_size, num_classes)
+    model = nn.Sequential(
+        nn.Linear(input_size, 256),
+        nn.ReLU(),
+        nn.Linear(256, num_classes)
+        )
+    optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+
+    model = model.to(device=device)  # move the model parameters to CPU/GPU
+    for epoch in range(epochs):
+        print(f'Current, start epoch {epoch+1}')
+        for batch in tqdm(loader_train):
+            x ,y = batch
+            model.train()  # put model to training mode
+            x = x.to(device=device, dtype=dtype)  # move to device, e.g. GPU
+            y = y.to(device=device, dtype=torch.long)
+
+            scores = model(x)
+            # print( f'shape of scores is {scores.shape}, while shape of y is {y.shape}')
+            loss = F.cross_entropy(scores, y)/args.batch_size
+
+            # Zero out all of the gradients for the variables which the optimizer
+            # will update.
+            optimizer.zero_grad()
+
+            # This is the backwards pass: compute the gradient of the loss with
+            # respect to each  parameter of the model.
+            loss.backward()
+
+            # Actually update the parameters of the model using the gradients
+            # computed by the backwards pass.
+            optimizer.step()
+
+        train_num_correct,train_num_samples =check_accuracy(loader_train,model)
+        train_epoch_acc = float(train_num_correct) / train_num_samples
+        
+        val_num_correct,val_num_samples =check_accuracy(loader_val,model)
+        val_epoch_acc = float(val_num_correct) / val_num_samples
+        print('Training ACC: Got %d / %d correct (%.2f)' % (train_num_correct, train_num_samples, 100 * train_epoch_acc))
+        print('Val ACC: Got %d / %d correct (%.2f)' % (val_num_correct, val_num_samples, 100 * val_epoch_acc))
+        print('-'*100)
+
+def check_accuracy(loader, model,print=False):
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    dtype = torch.float32
+
+    num_correct = 0
+    num_samples = 0
+    model.eval()  # set model to evaluation mode
+    with torch.no_grad():
+        for batch in tqdm(loader):
+            x,y =batch
+            x = x.to(device=device, dtype=dtype)  # move to device, e.g. GPU
+            y = y.to(device=device, dtype=torch.long)
+            scores = model(x)
+            _, preds = scores.max(1)
+            num_correct += (preds == y).sum()
+            num_samples += preds.size(0)
+    if print:
+        epoch_acc = float(num_correct) / num_samples
+        print('Got %d / %d correct (%.2f)' % (num_correct, num_samples, 100 * epoch_acc))
+    return num_correct,num_samples
+
+
+def seed_everything(seed=11711):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.benchmark = False
+    torch.backends.cudnn.deterministic = True
+
+def get_args():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--seed", type=int, default=11711)
+    parser.add_argument("--epochs", type=int, default=10)
+
+    parser.add_argument("--use_gpu", action="store_true")
+
+    # hyper parameters
+    parser.add_argument(
+        "--batch_size",
+        help="default 8",
+        type=int,
+        default=8,
+    )
+    parser.add_argument("--hidden_dropout_prob", type=float, default=0.3)
+    parser.add_argument(
+        "--lr",
+        type=float,
+        help="learning rate, default lr 1e-5",
+        default=1e-5,
+    )
+
+    args = parser.parse_args()
+    return args
 
 if __name__ == "__main__":
+    args = get_args()
+    seed_everything(args.seed)
+    train(args)
     
