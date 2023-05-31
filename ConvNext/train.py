a9c512b3c922757143904812f5e5f8f0424ceb85
jojamison <jojamison@davidson.edu>
2023-05-31 15:37:04 -0700

    updated train.py

--- ConvNext/train.py
+++ ConvNext/train.py
@@ -195,7 +195,6 @@ def train(args):
         # print(x.size())
             scores = model(x)[0]
             # print( f'shape of scores is {len(scores)}, while shape of y is {y.shape}')
-            # print(f"type of scores[0] is {scores[0].type}")
             
             loss = F.cross_entropy(scores, y)/args.batch_size
 
