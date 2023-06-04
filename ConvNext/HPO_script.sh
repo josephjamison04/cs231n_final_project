dc3f9a4e82d4bf89e7de88b0802ef73ea6af1cbd
jojamison <jojamison@davidson.edu>
2023-06-03 17:46:29 -0700

    updated training script

--- ConvNext/HPO_script.sh
+++ ConvNext/HPO_script.sh
@@ -1,7 +1,7 @@
 echo "Beginning training script..."
 
 
-python train.py --use_gpu --batch_size 64 --lr 1e-4 --epochs 1 --option convNext --from_pretrain
+python train.py --use_gpu --batch_size 64 --epochs 1 --option convNext --from_pretrain
 
 
 echo "WARNING! SYSTEM WILL SHUTDOWN IN 60 SECONDS UNLESS CTRL-C INTERRUPT..."
