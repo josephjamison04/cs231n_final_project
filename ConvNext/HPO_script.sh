c8956864f891dd56b842ec1d46dda82b9ff9e2c0
jojamison <jojamison@davidson.edu>
2023-06-04 21:09:37 -0700

    update training script

--- ConvNext/HPO_script.sh
+++ ConvNext/HPO_script.sh
@@ -1,7 +1,7 @@
 echo "Beginning HPO training script with hyperparamters specified in train.py..."
 
 
-python train.py --use_gpu --batch_size 128 --epochs 10 --option convNext --from_pretrain --small_data
+python train.py --use_gpu --batch_size 128 --epochs 1 --option convNext --from_pretrain --small_data
 
 # python train.py --use_gpu --batch_size 64 --epochs 10 --option convNext --from_pretrain
 
