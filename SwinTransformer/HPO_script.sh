d2446b4cefb75aeba048d6d141ee17e47a5ccfc8
Haoyu Liu <haoyuliu@stanford.edu>
2023-06-07 00:10:29 +0000

    updated training for swin

--- SwinTransformer/HPO_script.sh
+++ SwinTransformer/HPO_script.sh
@@ -1,7 +1,8 @@
 echo "Beginning HPO training script with hyperparamters specified in train.py..."
 
 
-python train.py --use_gpu --batch_size 32 --epochs 5 --option swin --from_pretrain
+# python train.py --use_gpu --batch_size 32 --epochs 5 --option swin --from_pretrain
+python train.py --use_gpu --batch_size 32 --epochs 10 --option swin
 
 
 echo "WARNING! SYSTEM WILL SHUTDOWN IN 60 SECONDS UNLESS CTRL-C INTERRUPT..."
